ecd7410923dd8f369092ccd4456a9be194a8242b
R5zzz <1602315416@qq.com>
2022-09-22 22:00:58 +0800

    完成后端对邮箱和用户名的验证，以及提供消息码

--- src/main/java/com/lenovo/travelspringboot/domain/User.java
+++ src/main/java/com/lenovo/travelspringboot/domain/User.java
@@ -1,5 +1,9 @@
 package com.lenovo.travelspringboot.domain;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 /**
@@ -7,12 +11,16 @@ import java.io.Serializable;
  */
 public class User implements Serializable {
     private int uid;//用户id
+    @Pattern(regexp = "(^[a-zA-Z0-9_-]{3,16}$)|(^[\\u2E80-\\u9FFF]{2,5})",message = "必须是英文和数字3-16位或中文2-5位")
     private String username;//用户名，账号
     private String password;//密码
+    @NotEmpty(message = "name不能为空！")
+    @Length(min = 5,max = 10,message = "name长度不够！")
     private String name;//真实姓名
     private String birthday;//出生日期
     private String sex;//男或女
     private String telephone;//手机号
+    @Pattern(regexp = "^([a-zA-Z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$",message = "邮箱格式不正确!")
     private String email;//邮箱
     private String status;//激活状态，Y代表激活，N代表未激活
     private String code;//激活码（要求唯一）
@@ -127,5 +135,21 @@ public class User implements Serializable {
 
     public void setCode(String code) {
         this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", birthday='" + birthday + '\'' +
+                ", sex='" + sex + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", email='" + email + '\'' +
+                ", status='" + status + '\'' +
+                ", code='" + code + '\'' +
+                '}';
     }
 }
