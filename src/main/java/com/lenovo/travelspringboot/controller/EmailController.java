912bd2bdff6559427e1bce21b5076e13651cdec7
R5zzz <1602315416@qq.com>
2022-09-29 20:30:24 +0800

    loading...提示框，完成，人性化交互更进一步！

--- src/main/java/com/lenovo/travelspringboot/controller/EmailController.java
+++ src/main/java/com/lenovo/travelspringboot/controller/EmailController.java
@@ -5,11 +5,14 @@ import com.lenovo.travelspringboot.service.EmailService;
 import com.lenovo.travelspringboot.service.UserHandleService;
 import com.lenovo.travelspringboot.util.RedisUtil;
 import com.lenovo.travelspringboot.util.UuidUtil;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EmailController {
@@ -39,8 +42,8 @@ public class EmailController {
         *   3.若激活成功=status=Y,第一次保存数据时候是默认为N
         *   4.前端提示信息
         * */
-        System.out.println(email);
-        System.out.println(username);
+//        System.out.println(email);
+//        System.out.println(username);
 
         String code = UuidUtil.getUuid().substring(0, 5);
 
@@ -58,15 +61,10 @@ public class EmailController {
 
         }
 
-//        System.out.println("emailCode:"+emailCode);
-//        System.out.println("email:"+email);
-
-
-//        return Msg.success();
     }
 
     private Msg getMsg(String emailCode, String email, String username, String key) {
-        emailService.sendMessage("1602315416@qq.com", email, key);
+//        emailService.sendMessage("1602315416@qq.com", email, key);
         boolean equals = emailCode.equals(key);
         if (equals){
 //            userHandleService.updateStatue("Y", username, email);
@@ -75,5 +73,6 @@ public class EmailController {
             return Msg.fail().add("activeStatus", "激活失败！");
         }
     }
+
 
 }
